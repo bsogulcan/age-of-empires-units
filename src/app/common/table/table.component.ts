159d394d787112053100de039f480185cbc62bdf
Suat Oğulcan Özcan <bsogulcan@gmail.com>
2024-10-05 21:03:27 +0300

    feat: pagination table

--- src/app/common/table/table.component.ts
+++ src/app/common/table/table.component.ts
@@ -1,15 +1,20 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TableColumn} from "./models/table-column";
 import {RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
 
 @Component({
     selector: 'app-table',
     standalone: true,
     imports: [
-        RouterLink
+        RouterLink,
+        FormsModule,
+        NgClass
     ],
     templateUrl: './table.component.html',
-    styleUrl: './table.component.scss'
+    styleUrl: './table.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent implements OnInit {
 
@@ -29,14 +34,43 @@ export class TableComponent implements OnInit {
     @Output()
     public onRowSelected = new EventEmitter<any>();
 
+    rows: Array<any> = [];
+
     skip: number = 0;
-    take: number = 0;
-    displayCount: 5 | 10 | 25 | 50 = 25;
+    displayCount: 10 | 25 | 50 = 10;
+    selectedPageIndex: number = 0;
     pages: Array<number> = [];
 
-
     ngOnInit(): void {
-        const pageCount = Math.floor(this.entities.length / this.displayCount);
-        console.log('Page Count:', pageCount)
+        this.initialPages();
     }
+
+    onDisplayCountChanged() {
+        this.initialPages();
+    }
+
+    initialPages() {
+        this.pages = [];
+        const pageCount = Math.floor(this.entities.length / this.displayCount);
+        for (let i = 0; i <= pageCount; i++) {
+            this.pages.push(i);
+        }
+
+        this.filterPage(0);
+    }
+
+    filterPage(pageIndex: number) {
+        if (pageIndex < 0) {
+            return;
+        }
+
+        if (pageIndex > this.pages.length-1) {
+            return;
+        }
+
+        this.selectedPageIndex = pageIndex;
+        this.skip = pageIndex * this.displayCount;
+        this.rows = [...this.entities].splice(this.skip, this.displayCount);
+    }
+
 }

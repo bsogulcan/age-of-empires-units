3b48a1559f87a71096350daeb1300e583e9f952e
Suat Oğulcan Özcan <bsogulcan@gmail.com>
2024-10-05 11:47:29 +0300

    feat: tailwind, dark theme and rubik font face

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'age-of-emipres-units';
+export class AppComponent implements OnInit {
+
+    ngOnInit(): void {
+        this.initialDarkTheme();
+    }
+
+    initialDarkTheme() {
+        const theme = localStorage.getItem('theme');
+
+        if (!theme || theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }
 }
